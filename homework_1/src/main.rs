abbf2cb7581f065dda824330a27c1c85c00d1d69
JuanCdeleon <143768170+JuanCdeleon@users.noreply.github.com>
2025-09-17 15:33:16 +0000

    My First homework changed

--- homework_1/src/main.rs
+++ homework_1/src/main.rs
@@ -2,13 +2,13 @@
 
 fn fahrenheit_to_celsius(f: f64) -> f64{
     //temp converter
-    let resultCel = (f - 32.0) * (5.0 / 9.0);
-    return resultCel;
+    let result_cel = (f - 32.0) * (5.0 / 9.0);
+    return result_cel;
 
 }
 fn celsius_to_fahrenheit(c: f64) -> f64{
-    let resultFar = (c * ( 9.0/5.0)) + 32.0;
-    return resultFar;
+    let result_far = (c * ( 9.0/5.0)) + 32.0;
+    return result_far;
 }
 
 fn main(){
