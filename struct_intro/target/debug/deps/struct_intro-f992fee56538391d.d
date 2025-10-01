6f9aea1a4fd1f4d8d06445dd515b93c6a219744f
JuanCdeleon <143768170+JuanCdeleon@users.noreply.github.com>
2025-10-01 22:04:52 +0000

    struct intro practice in class completed

---
+++ struct_intro/target/debug/deps/struct_intro-f992fee56538391d.d
@@ -0,0 +1,5 @@
+/workspaces/rustfall2025/struct_intro/target/debug/deps/struct_intro-f992fee56538391d.d: src/main.rs
+
+/workspaces/rustfall2025/struct_intro/target/debug/deps/libstruct_intro-f992fee56538391d.rmeta: src/main.rs
+
+src/main.rs:
