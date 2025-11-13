77b57ab08cc269a5a871852fb22a373559822aa9
JuanCdeleon <143768170+JuanCdeleon@users.noreply.github.com>
2025-11-13 00:39:12 +0000

    inclass assignment done

---
+++ inclass_traits/target/debug/deps/inclass_traits-6367ad9826735341.d
@@ -0,0 +1,5 @@
+/workspaces/rustfall2025/inclass_traits/target/debug/deps/inclass_traits-6367ad9826735341.d: src/main.rs
+
+/workspaces/rustfall2025/inclass_traits/target/debug/deps/inclass_traits-6367ad9826735341: src/main.rs
+
+src/main.rs:
---
+++ inclass_traits/target/debug/inclass_traits.d
@@ -0,0 +1,1 @@
+/workspaces/rustfall2025/inclass_traits/target/debug/inclass_traits: /workspaces/rustfall2025/inclass_traits/src/main.rs
